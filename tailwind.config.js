014139e6a6878d81d754c42b3fab6b4ac7d08647
Abel <abel.dieguez@wakasolutions.com>
2023-08-13 11:13:34 +0200

    Add tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
         "primary": "#B2D0C6",
         "secondary": "#FEB15F",
         "terciary": "#1B5F5E",
-        "accent": "#D84531"
+        "accent": "#D84531",
+        "rainbox-green": "#96CB93",
+        "rainbox-natilla": "#EADA93",
+        "rainbox-yellow": "#F3C846",
+        "rainbox-orange": "#EEA71B",
+        "rainbox-red": "#ED4E38",
       },
       fontFamily: {
         indie: ['Indie Flower', 'cursive'],
