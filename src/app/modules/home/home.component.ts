d94619bf33b1ad05c4c48b1f1d5c53fe592e3de9
Abel <abel.dieguez@wakasolutions.com>
2023-08-14 13:07:18 +0200

    animation and responsive cover

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewChildren, ElementRef, OnInit, QueryList } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  public socialList = [
+    {
+      icon: 'assets/icons/instagram.svg',
+      url: 'www.instagram.com'
+    },
+    {
+      icon: 'assets/icons/spotify.svg',
+      url: 'www.spotify.com'
+    },
+    {
+      icon: 'assets/icons/facebook.svg',
+      url: 'www.facebook.com'
+    },
+  ]
+
+  @ViewChild('coverImage', {static: true })
+  coverImage!: ElementRef<HTMLDivElement>;
+
+  @ViewChildren('image')
+  image!: QueryList<ElementRef>;
+
+  ngOnInit() {
+  }
+  
+  ngAfterViewInit() {
+    this._initialAnimations();
+
+  }
+
+  private _initialAnimations() {
+    // gsap.to(this.coverImage.nativeElement, {
+    //   duration: 1,
+    //   rotate: 10
+    // });
+  }
 }
